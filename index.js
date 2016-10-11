40e07500a9e0919f591e09f999a528c08e5c4ca6
Alexei Moisseev <alexmois@ya.ru>
2016-10-11 12:29:15 +0300

    Added close event capture, new Date() -> Date.now()

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function decorator(requestModule, trigger) {
     var oldRequest = requestModule.request;
     function end(options, timers, err, res) {
-        timers.total = Math.round(timers.end.getTime() - timers.socket.getTime());
+        timers.total = Math.round(timers.end - timers.socket);
         trigger({
             timers: timers,
             code: err ? getCode(err.code) : res.statusCode,
@@ -11,27 +11,33 @@ function decorator(requestModule, trigger) {
     }
     function request(options, callback) {
         var timers = {
-            start: new Date()
+            start: Date.now()
         };
         var req = oldRequest(options, function (res) {
-            timers.ttfb = new Date();
+            timers.ttfb = Date.now();
             res.on('data', function () {
                 // this needs to set end handler
             });
             res.on('end', function () {
-                timers.end = new Date();
+                timers.end = Date.now();
                 end(options, timers, null, res);
+            });
+            res.on('close', function (err) {
+                timers.end = Date.now();
+                end(options, timers, {
+                    code: 503
+                });
             });
             return callback(res);
         });
         req.on('socket', function () {
-            timers.socket = new Date();
+            timers.socket = Date.now();
         });
         req.on('response', function () {
-            timers.response = new Date();
+            timers.response = Date.now();
         });
         req.on('error', function (err) {
-            timers.end = new Date();
+            timers.end = Date.now();
             end(options, timers, err);
         });
         return req;
@@ -40,7 +46,7 @@ function decorator(requestModule, trigger) {
 }
 
 function getCode(error) {
-    return 504;
+    return typeof error === 'number' ? error : 504;
 }
 
 module.exports = decorator;
